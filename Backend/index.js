8e3bdb8b97425bc714fc7fc164c5cfcd11338c2a
tajbiulrawol@gmail.com <tajbiulrawol@gmail.com>
2024-08-29 23:33:30 +0600

    added complete crud operations

--- Backend/index.js
+++ Backend/index.js
@@ -12,19 +12,74 @@ let currentId = 1;
 
 // Create a new blog post
 app.post('/api/posts',(req,res)=>{
-    console.log('POST request received');
-    console.log(req.body);  // Correct way to log the incoming request body
-    res.status(201).json({ message: 'Post created', data: req.body });
-    posts.push(req.body);
+    const {id,title,content,author,created_at} = req.body;
+    if (!title || !id || !author ) {
+        return res.status(400).json({error:`Data cannot be empty ${req.body}`})
+    }
+
+    // Check if a post with the same ID already exists
+    const existingPost = findPost(id);
+    if (existingPost) {
+        return res.status(400).json({ error: `A post with ID ${id} already exists.` });
+    }
+
+
+    const newPost = {
+        id: currentId++,
+        title,
+        content,
+        author,
+        created_at: new Date().now
+    }
+    posts.push(newPost);
+    res.status(201).json({ message: 'Post created', data: newPost });
 })
 
 // Read a single blog post by id
 app.get('/api/posts/:id', (req, res) => {
-    console.log('GET request received for post with ID:', req.params.id);
-    res.status(200).json({ message: 'GET request reached', data: posts });
-    console.log(posts);
+    const post = findPost(req.params.id);
+    if (!post) {
+        res.status(404).json({error: "404 Post not found"})
+    }
+    res.json(post);
   });
 
+
+// Return all the posts 
+app.get('/api/posts/', (req, res) => {
+    res.status(200).json(posts);
+  });
+
+
+// Update a blog post by id
+app.put('/api/posts/:id', (req, res) => {
+    const post = findPost(req.params.id);
+    if (!post) {
+      return res.status(404).json({ error: 'Post not found' });
+    }
+    res.json(updatePost(req.body))
+  }); 
+//delete posts
+app.delete('/api/posts/:id',(req,res)=>{
+    const postIndex = posts.findIndex(p=>p.id === parseInt(req.params.id));
+    if (postIndex < 0) {
+        return res.status(404).json({error:"Post not found"})
+    }
+    posts.splice(postIndex,1);
+    res.status(204).send("post deleted");
+})
+
+const findPost = (id)=>{
+    return posts.find(p=>p.id === parseInt(id));
+}
+
+const updatePost = (post)=>{
+    const { title, content, author } = post;
+    if (title) post.title = title;
+    if (content) post.content = content;
+    if (author) post.author = author;
+    return post;
+}
 
 app.listen(PORT,()=>{
     console.log(`Server running on http://localhost:${PORT}`);
