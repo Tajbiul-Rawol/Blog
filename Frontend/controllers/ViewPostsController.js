c5648c9fd8707fac9a8d5ef2248349d1e7ef3b46
tajbiulrawol@gmail.com <tajbiulrawol@gmail.com>
2024-09-01 14:29:54 +0600

    refactored front end and added a layout with angular routing. Added views CreatePost, viewPosts.

---
+++ Frontend/controllers/ViewPostsController.js
@@ -0,0 +1,48 @@
+app.controller('ViewPostsController', function($scope, $http) {
+    $scope.posts = [];
+    $scope.selectedPost = {};
+
+    $scope.fetchPosts = function() {
+        $http.get(`${BASE_URL}/api/posts`).then(response => {
+            $scope.posts = response.data;
+        });
+    };
+
+    $scope.deletePost = function(id) {
+        if(confirm('Are you sure you want to delete this post?')) {
+            $http.delete(`${BASE_URL}/api/posts/${id}`).then(response => {
+                $scope.posts = $scope.posts.filter(p => p.id !== id);
+            }).catch(error => {
+                alert('Error deleting post: ' + error.data.error);
+            });
+        }
+    };
+
+    // Update an existing post
+    $scope.selectPost = function(post) {
+        $scope.selectedPost = angular.copy(post); // Use angular.copy to avoid direct binding
+    };
+
+    //open the modal
+    $scope.openModal = function(post){
+        $scope.selectPost(post);
+        $('#updateModal').modal('show');
+    };
+
+    $scope.updatePost = function () {
+        
+        console.log($scope.selectedPost);
+        if (!$scope.selectedPost) return;
+
+        $http.put(`${BASE_URL}/api/posts/${$scope.selectedPost.id}`, $scope.selectedPost).then(response => {
+            const index = $scope.posts.findIndex(p => p.id === $scope.selectedPost.id);
+            $scope.posts[index] = response.data;
+            $scope.resetForm(); // Reset the form
+        }).catch(error => {
+            alert('Error updating post: ' + error.data.error);
+        });
+    }
+
+
+    $scope.fetchPosts(); // Fetch posts on controller initialization
+});
