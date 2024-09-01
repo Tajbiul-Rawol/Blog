278630ba6966ca0a28284d8393548a2ec1864712
tajbiulrawol@gmail.com <tajbiulrawol@gmail.com>
2024-09-01 20:24:31 +0600

    added logging for all the transactions made to the post tables

--- Backend/controllers/postController.js
+++ Backend/controllers/postController.js
@@ -45,13 +45,15 @@ const PostController = {
 
             const updatedPost = await PostModel.update(req.params.id, req.body);
             
-            const logResult = await LogModel.log('Update Post', `Post updated with title: ${req.body.title}`, req.body.author);
-            if(!logResult){
-                res.status(400).json({ error: err.message });
-            }
+            
 
             if (!updatedPost) {
                 return res.status(400).json({ error: 'Failed to update the post' });
+            }
+
+            const logResult = await LogModel.log('Update Post', `Post updated with title: ${req.body.title}`, req.body.author);
+            if(!logResult){
+                res.status(400).json({ error: err.message });
             }
             res.status(200).json(updatedPost);
         } catch (err) {
@@ -68,13 +70,20 @@ const PostController = {
             }
 
             const deletedPost = await PostModel.delete(req.params.id);
+            
 
             if (!deletedPost) {
                 return res.status(404).json({ error: 'Deletion unsuccessful' }); // Ensure correct response if no post was deleted
             }
 
+            const logResult = await LogModel.log('Delete Post', `Post deleted with title: ${req.body.title}`, req.body.author);
+            if(!logResult){
+                res.status(400).json({ error: err.message });
+            }
+
             res.status(200).send("Post deleted successfully");
         } catch (err) {
+            const result = await LogModel.log('Error', err, req.body.author);
             res.status(400).json({ error: err.message });
         }
     }
